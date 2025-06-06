da7a9f93103fba8dca7098d396c61e79680abd0f
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-06 15:01:21 -0700

    adjust scroller to account for fixed header

--- src/components/root/Scroller.js
+++ src/components/root/Scroller.js
@@ -2,18 +2,20 @@ import { useEffect } from 'react'
 import { useWindowDimensions } from '../../hooks/useWindowDimensions'
 import style from './root.module.css'
 
-export const Scroller = ({text, totalHeight, position, color, handleWidthChange}) => {
+export const Scroller = ({text, totalHeight, position, color, handleWidthChange, headerHeight}) => {
     const {height}= useWindowDimensions()
-    const textHeight = height/text.length
+    const textHeight = (height - headerHeight)/text.length
     const textWidth = textHeight*.7
-    const hlLength = 100*height/totalHeight
+    const hlLength = 100*(height-headerHeight)/totalHeight
     const hlStart = 100*position/totalHeight
     useEffect(()=>{
         handleWidthChange(textWidth)
     }, [handleWidthChange, textWidth])
     
     return (
-        <div className={style.scroller}>
+        <div className={style.scroller}
+            style={{top: headerHeight}}
+            >
            <p
            style={{
             fontSize: `${textHeight}px`,
