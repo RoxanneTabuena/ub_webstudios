1b93658f4d1453bc9152e4c6acf04caee0bd6b87
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-10 12:53:42 -0700

    link logo to homepage

--- src/components/root/Header.js
+++ src/components/root/Header.js
@@ -1,12 +1,13 @@
+import { NavLink } from 'react-router-dom'
 import { Nav } from './nav/Nav'
 import logo from '../../assets/img/Logo_white 2.png'
 export const Header = ({headerRef}) => {
     return (
         <header ref={headerRef}>
-            <div>
+            <NavLink to="/">            
                 <img src={logo} alt="unbound studio logo"></img>
                 <h3>Web Studio</h3>
-            </div>
+            </NavLink>
             <Nav />
         </header>
     )
