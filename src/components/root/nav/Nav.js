cb05b17409825cd15bad9a53a46ba2f992ac01ae
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-13 09:43:00 -0700

    update styling for desktop

--- src/components/root/nav/Nav.js
+++ src/components/root/nav/Nav.js
@@ -1,9 +1,11 @@
+import { useOrientation } from "../../../hooks/useOrientation"
 import { useState, useEffect } from "react"
 import { useLocation } from "react-router-dom"
 import { NavLink } from "react-router-dom"
 import { MenuIcon } from "./MenuIcon"
 import style from '../nav/nav.module.css'
 export const Nav = () => {
+    const orientation= useOrientation()
     const [open, setOpen] = useState(false)
     const {pathname} = useLocation()
     // close menu on path change
@@ -14,31 +16,39 @@ export const Nav = () => {
     const handleMenuToggle= () => {
         setOpen(!open)
     }
-
+    const nav = [
+        <NavLink to="/about">
+            <h3>About</h3>
+        </NavLink>,
+        <NavLink to="/pricing">
+            <h3>Pricing</h3>
+        </NavLink>,
+        <NavLink to="/services">
+            <h3>Services</h3>
+        </NavLink>,
+        <NavLink to="/work">
+            <h3>Work</h3>
+        </NavLink>
+    ]
     const expand = (
         <div className={style.menu}>
-            <div>
-                <NavLink to="/about">
-                    <h3>About</h3>
-                </NavLink>
-                <NavLink to="/services">
-                    <h3>Services</h3>
-                </NavLink>
+            <div className={style.mobMenu}>
+                {nav}
             </div>
-            <div>
-                <NavLink to="/pricing">
-                    <h3>Pricing</h3>
-                </NavLink>
-                <NavLink to="/work">
-                    <h3>Work</h3>
-                </NavLink>
-            </div>
+
             <MenuIcon open={true} handleMenuToggle={handleMenuToggle}/>
         </div>
     )
     const contract = (
         <MenuIcon open={false} handleMenuToggle={handleMenuToggle}/>
     )
+    if(orientation === 'landscape'){
+        return(
+            <nav className={style.deskMenu}>
+                {nav}
+            </nav>
+        )
+    }
     return (
         <nav>
             {open === true ? expand : contract} 
