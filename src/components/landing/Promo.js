b8022908f50a6fba573867ad5fe20a2fb317f397
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-13 09:16:14 -0700

    create promo section files

---
+++ src/components/landing/Promo.js
@@ -0,0 +1,10 @@
+import { PromoCard } from './PromoCard'
+import style from './landing.module.css'
+
+export const Promo = () => {
+    return (
+        <div className={style.promo}>
+           Promo
+        </div>
+    )
+}
---
+++ src/components/landing/PromoCard.js
@@ -0,0 +1,9 @@
+import style from './landing.module.css'
+
+export const PromoCard = () => {
+    return (
+        <div className={style.promoCard}>
+            promoCard
+        </div>
+    )
+}
