610f1ac15611d71e344f3b9a74dc140acf2c1c79
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-13 15:29:41 -0700

    create card flipping animation

--- src/components/landing/PromoCard.js
+++ src/components/landing/PromoCard.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from 'react'
+import { useOrientation } from '../../hooks/useOrientation'
 import style from './landing.module.css'
 
-export const PromoCard = () => {
+export const PromoCard = ({title, src, text, queue, length}) => {
+    const [flip, setFlip] = useState(false)
+    const [abort, setAbort] = useState(false)
+    const orientation = useOrientation()
+
+    useEffect(()=>{
+        if(!abort){
+            if(orientation==='landscape'){
+                const turn_1 = setTimeout(()=>{
+                    setFlip(true)
+                    const turn_2 = setTimeout(()=>{
+                        setFlip(false)
+                    }, 1500*[length-queue]+4500)
+                    return() => clearTimeout(turn_2)
+                }, queue*1500)
+                return () => clearTimeout(turn_1)
+            }
+            else{
+                const turn = setTimeout(()=>{
+                    setFlip(true)
+                }, 1200)
+                return ()=> clearTimeout(turn)
+            }
+        }
+    },[queue])
     return (
-        <div className={style.promoCard}>
-            promoCard
+        <div className={style.cardContainer} onMouseEnter={()=>{setAbort(true)}}>
+            <div className={`${style.promoCard} ${flip ? style.flipped : ''}`}>
+                <div className={style.cardFront}>
+                    <img src={src} alt={`${title} icon`} />
+                </div>
+                <div className={style.cardBack}>
+                    <h3>{title}</h3>
+                    <p>{text}</p>
+                </div>
+            </div>
         </div>
     )
 }
