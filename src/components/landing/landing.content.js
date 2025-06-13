09733ebc0f88c255380c54be026f032cb98dc8a6
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-13 15:29:16 -0700

    add promo section

--- src/components/landing/landing.content.js
+++ src/components/landing/landing.content.js
@@ -1,3 +1,14 @@
+import mobileFirst from '../../assets/img/Close_Icon.png'
+import fullyResponsive from '../../assets/img/Close_Icon.png'
 export const promo_info = {
-    
+    "Mobile First":{
+        text: 
+        `We start building your site for mobile devices first making sure the code is as lean and optimized with no bloated waste and makes the site even more responsive.`,
+        src: mobileFirst
+    },
+    "Fully Responsive":{
+        text: 
+        `Your website will fit all mobile screens sizes, tablets, and desktop sizes so visitors can access your site from anywhere and see a beautiful site no matter the screen size.`,
+        src: fullyResponsive
+    }
 }
