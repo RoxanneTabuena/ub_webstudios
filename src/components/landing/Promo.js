9d5df6849752c9d728ba25db0014b12c8ecbbffd
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-13 15:30:05 -0700

    map promo info into card components

--- src/components/landing/Promo.js
+++ src/components/landing/Promo.js
@@ -1,10 +1,22 @@
+import { promo_info } from './landing.content'
 import { PromoCard } from './PromoCard'
 import style from './landing.module.css'
 
 export const Promo = () => {
+    const titleList = Object.keys(promo_info)
+    const cards = titleList.map((title, i)=>{
+        return <PromoCard 
+                    title={title} 
+                    text={promo_info[title].text} 
+                    src={promo_info[title].src}
+                    key={`${title}${i}`}
+                    queue={i+1}
+                    length={titleList.length}
+                    />
+    })
     return (
         <div className={style.promo}>
-           Promo
+            {cards}
         </div>
     )
 }
