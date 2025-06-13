6ea1a7122bc68d84ff0f94da0e2dc2b9c4f75cb3
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-13 15:28:55 -0700

    order display according to orientation

--- src/components/landing/Banner.js
+++ src/components/landing/Banner.js
@@ -1,12 +1,11 @@
 import { useOrientation } from '../../hooks/useOrientation'
 import { NavButton } from '../../blocks/NavButton'
-import demo from '../../assets/img/Demo_Pic.png'
+import demoGif from '../../assets/img/Demo_Pic.png'
 import style from './landing.module.css'
 
 export const Banner = () => {
     const orientation = useOrientation()
-    return (
-        <div className={style.banner}>
+    const head = 
             <div className={style.bannerHead}>
                 <div className={style.left}>
                     <h4>next-level</h4>
@@ -20,13 +19,32 @@ export const Banner = () => {
                     </div>
                 </div>
             </div>
-            <img src={demo} alt="a laptop and phone showing responsive versions of the same website"></img>
-            <p style={{textAlign: `${orientation === 'portrait' && 'center'}`}}>No page builders or WordPress. We offer 100% hand-coded websites with superior results starting at $175/mo as well as Google Ads and SEO services.</p>
+    const demo = 
+        <img src={demoGif} alt="a laptop and phone showing responsive versions of the same website"></img>
+    const text = 
+        <p>No page builders or WordPress. We offer 100% hand-coded websites with superior results starting at $175/mo as well as Google Ads and SEO services.</p>
+    const links = 
             <div className={style.bannerButts}>
                 <NavButton text="start quote" path="/book"/>
                 <NavButton text="about studio" path="/about" backgroundColor="fuchsia"/>
             </div>
-        </div>
+    return (
+            orientation === 'portrait'?
+                <div className={style.banner}>
+                    {head}
+                    {demo}
+                    {text}
+                    {links}
+                </div> 
+            :
+                <div className={style.banner}>
+                    <div className={style.bannerLeft}>
+                        {head}
+                        {text}
+                        {links}
+                    </div>
+                    {demo}
+                </div>        
     )
 }
 
