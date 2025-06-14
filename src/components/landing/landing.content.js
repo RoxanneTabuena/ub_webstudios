056119a12bd90343ab17c998d953233270d89e48
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-14 11:16:40 -0700

    add content

--- src/components/landing/landing.content.js
+++ src/components/landing/landing.content.js
@@ -1,14 +1,34 @@
 import mobileFirst from '../../assets/img/Close_Icon.png'
 import fullyResponsive from '../../assets/img/Close_Icon.png'
 export const promo_info = {
-    "Mobile First":{
+    "Universally Accessible":{
         text: 
         `We start building your site for mobile devices first making sure the code is as lean and optimized with no bloated waste and makes the site even more responsive.`,
         src: mobileFirst
     },
-    "Fully Responsive":{
+    "Optimized Page Speed":{
+        text: 
+        `If your site takes more than 3 seconds to load you can lose up to 50% your traffic because it took too long. Our sites load 1 second or less ensuring everyone that visits your site reaches it.`,
+        src: mobileFirst
+    },
+    "Next-Level Custom Graphics":{
+        text: 
+        `If your site takes more than 3 seconds to load you can lose up to 50% your traffic because it took too long. Our sites load 1 second or less ensuring everyone that visits your site reaches it.`,
+        src: mobileFirst
+    },
+    "Committed brand Translation":{
         text: 
         `Your website will fit all mobile screens sizes, tablets, and desktop sizes so visitors can access your site from anywhere and see a beautiful site no matter the screen size.`,
         src: fullyResponsive
+    },
+    "Community Rooted":{
+        text: 
+        `We do not hire cheap overseas developers. We are mostly based in the USA with team members in Washington, Texas, Pennsylvania, and the UK and work 100% remote from home.`,
+        src: mobileFirst
+    },
+    "Fully Translated":{
+        text: 
+        `We do not hire cheap overseas developers. We are mostly based in the USA with team members in Washington, Texas, Pennsylvania, and the UK and work 100% remote from home.`,
+        src: mobileFirst
     }
 }
