8dbfd3c7164f98ce70231617ab6c895422e8ee72
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-16 13:46:37 -0700

    adjust card width according to screen size

--- src/components/landing/PromoCard.js
+++ src/components/landing/PromoCard.js
@@ -1,10 +1,21 @@
 import { useEffect, useState } from 'react'
 import { useOrientation } from '../../hooks/useOrientation'
+import { useWindowDimensions } from '../../hooks/useWindowDimensions'
 import style from './landing.module.css'
 
 export const PromoCard = ({title, src, text, queue, length, abort}) => {
     const [flip, setFlip] = useState(false)
     const orientation = useOrientation()
+    const {width} = useWindowDimensions()
+    const cardWidth = orientation === 'landscape' ? Math.floor([width*.9]/3): width*.75
+    const cardStyle = {
+        perspective: `${cardWidth*1.2}px`,
+        width: `${cardWidth}px`,
+        height: `${cardWidth*8/7}px`,
+        margin: `${width*.01}px`,
+        zIndex: `0px`
+    }
+
     useEffect(()=>{
         if(!abort){
             if(orientation==='landscape'){
@@ -12,7 +23,7 @@ export const PromoCard = ({title, src, text, queue, length, abort}) => {
                     setFlip(true)
                     const turn_2 = setTimeout(()=>{
                         setFlip(false)
-                    }, 1500*[length-queue]+4500)
+                    }, 1500*[length-queue]+3000)
                     return() => clearTimeout(turn_2)
                 }, queue*1500)
                 return () => clearTimeout(turn_1)
@@ -25,8 +36,9 @@ export const PromoCard = ({title, src, text, queue, length, abort}) => {
             }
         }
     },[queue, abort])
+
     return (
-        <div className={style.cardContainer}>
+        <div className={style.cardContainer} style={cardStyle}>
             <div className={`${style.promoCard} ${flip ? style.flipped : ''}`}>
                 <div className={style.cardFront}>
                     <img src={src} alt={`${title} icon`} />
