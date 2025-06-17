532a734752c24bac32fc90b7846f8c839a4e8d0c
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-17 13:40:45 -0700

    add size option

--- src/blocks/NavButton.js
+++ src/blocks/NavButton.js
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router-dom"
 import style from './blocks.module.css'
 
-export const NavButton = ({path, text, textColor, backgroundColor}) => {
+export const NavButton = ({path, text, textColor, backgroundColor, size}) => {
     return (
             <NavLink to={path}>
                 <button style={{
                     backgroundColor: backgroundColor
-                }} className={style.navButton}>
+                }} className={`${style.navButton} ${style.size}`}>
                     <h3 style={{
                         color: textColor
                     }}>{text}</h3>
