d161a3a9c5eb9343f5c210139817a8dd81fcb848
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-20 08:50:58 -0700

    fiz sizing logic

--- src/blocks/NavButton.js
+++ src/blocks/NavButton.js
@@ -2,15 +2,16 @@ import { NavLink } from "react-router-dom"
 import style from './blocks.module.css'
 
 export const NavButton = ({path, text, textColor, backgroundColor, size, fontSize, font}) => {
+    const sizeStyle = size === 'large' ? style.large : style.small 
     return (
             <NavLink to={path}>
                 <button style={{
                     backgroundColor: backgroundColor
-                }} className={`${style.navButton} ${style.size}`}>
+                }} className={`${style.navButton} ${sizeStyle}`}>
                     <h4 style={{
                         color: textColor,
                         fontSize: `${fontSize && fontSize}`,
-                        fontFamily: `${font? font : 'Text'}`
+                        fontFamily: `${font? font : ''}`
                     }}>{text}</h4>
                 </button>
             </NavLink>
