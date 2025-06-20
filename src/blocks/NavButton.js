470e0bdb889fdb08a813cdd22e93ed414dcf0be1
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-20 08:12:37 -0700

    update font logic

--- src/blocks/NavButton.js
+++ src/blocks/NavButton.js
@@ -1,15 +1,17 @@
 import { NavLink } from "react-router-dom"
 import style from './blocks.module.css'
 
-export const NavButton = ({path, text, textColor, backgroundColor, size}) => {
+export const NavButton = ({path, text, textColor, backgroundColor, size, fontSize, font}) => {
     return (
             <NavLink to={path}>
                 <button style={{
                     backgroundColor: backgroundColor
                 }} className={`${style.navButton} ${style.size}`}>
-                    <h3 style={{
-                        color: textColor
-                    }}>{text}</h3>
+                    <h4 style={{
+                        color: textColor,
+                        fontSize: `${fontSize && fontSize}`,
+                        fontFamily: `${font? font : 'Text'}`
+                    }}>{text}</h4>
                 </button>
             </NavLink>
     )
