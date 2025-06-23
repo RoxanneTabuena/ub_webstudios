8649f18026e1473c1145fa0ceec4d33f0eb82861
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-23 14:12:46 -0700

    convert page info into relative nav

--- src/components/root/header/PageInfo.js
+++ src/components/root/header/PageInfo.js
@@ -1,12 +1,15 @@
 import { useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import style from './header.module.css'
 export const PageInfo = ({ height}) => {
     const {pathname} = useLocation()
-    const title = pathname === '/' ? 'Welcome' : pathname.split('/')[1].toUpperCase()
+    const title = pathname === '/' ? 'Welcome' : pathname.split('/')[1]
     return (
         <div className={style.pageInfo}
             style={{
                 height: height}}
         > 
-            <h2>{title}</h2>
+        <NavLink to={pathname !== '/' ? `/${title}` : '/'}>
+            <h2>{title.toUpperCase()}</h2>
+        </NavLink>
         </div>)}
