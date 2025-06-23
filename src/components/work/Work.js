56f69108b261fcbc6e6dddb4d7217766b3e44d08
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-23 14:07:11 -0700

    style according to filter height

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,13 +1,17 @@
 import { Filter } from './Filter'
 import { Clients } from './Clients'
+import { useState } from 'react'
 import style from './work.module.css'
 
 export const Work = () => {
-
+const [filterHeight, setFilterHeight] = useState(0)
+const handleFilterChange = (height) => {
+    setFilterHeight(height)
+}
     return (
         <div className={style.work}>
-            <Filter/>
-            <Clients />
+            <Filter handleFilterChange={handleFilterChange}/>
+            <Clients filterHeight={filterHeight}/>
         </div>
     )
 }
