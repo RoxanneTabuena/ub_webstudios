e9fbe2d075e38052fa70b660ae86463f99b78857
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-23 14:06:31 -0700

    adjust path logic to include multiple filters

--- src/components/work/Filter.js
+++ src/components/work/Filter.js
@@ -1,11 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useOutletContext, useLocation } from 'react-router-dom'
+import { useComponentHeight } from '../../hooks/useComponentHeight'
 import { client_info } from './work.content'
 import { NavButton } from '../../blocks/NavButton'
 import style from './work.module.css'
 
-export const Filter= () => {
+export const Filter= ({handleFilterChange}) => {
+    // get current filters through pathname
+    const {pathname} = useLocation()
+    // get header height to determine filter position
+    const headerHeight = useOutletContext()
     // control filter state
     const [open, setOpen] = useState()
+    // list current filters
+    const path = pathname.split('/').slice(2)
+    const determinePath = (tag) => {
+       if(!path.length){
+        return tag
+       }else{
+        return `/work/${path}&&${tag}`
+       }
+    }
     // generate filter list
     const clientNames = Object.keys(client_info)
     let tagsList = new Set()
@@ -20,14 +35,19 @@ export const Filter= () => {
         //     })
         // }
     })
+    const [ filterRef, filterHeight] = useComponentHeight()
+    // update filter height for clients positioning
+    useEffect(()=>{
+        handleFilterChange(filterHeight)
+    }, [filterHeight])
     return (
-        <div className={style.filter}>
+        <div className={style.filter} ref={filterRef} style={{top: headerHeight+10}}>
             {open? 
                 <div>
                     <h3 onClick={()=>setOpen(false)}>project types:</h3>
                         {Array.from(tagsList).map((tag, i)=>{
                             return (<p key={tag}>
-                            <NavButton path={`/work/${tag}`} text={tag} size={"small"} backgroundColor={"white"}/>
+                            <NavButton path={determinePath(tag)} text={tag} size={"small"} backgroundColor={"white"}/>
                             {i !== tagsList.size-1 && ","}
                                 </p>)
 
