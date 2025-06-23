ecd183ae0c9bee383f8ba34eabd70088a91e71a4
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-23 14:07:34 -0700

    adjust logic to accommodate multiple filters

--- src/components/work/Clients.js
+++ src/components/work/Clients.js
@@ -2,22 +2,27 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { client_info } from './work.content'
 import { Client } from './Client'
+import style from './work.module.css'
 
-export const Clients = () => {
-    const {filter} = useParams()
+export const Clients = ({filterHeight}) => {
+    let {filter} = useParams()
     const [feature, setFeature ] = useState(null)
     useEffect(()=>{},[feature])
     const handleSwitch = (client) => {
         setFeature(client)
     }
-    const clientNames = Object.keys(client_info)
-    let list = !filter ? clientNames : 
-            clientNames.filter((name)=>{
-                return client_info[name].tags.includes(filter)
-            })
+    if(filter){filter=filter.split('&&')}
+    let clientNames = Object.keys(client_info)
+    if(filter){
+        filter.forEach((tag)=>{
+        clientNames = clientNames.filter((name)=> {
+            return client_info[name].tags.includes(tag)
+        })
+    })}
     return ( 
-        <div>
-            {list.map((client)=>{
+        <div className={style.clients}
+            style={{paddingTop: filterHeight}}>
+            {clientNames.map((client)=>{
                 return <Client 
                             title={client}
                             key={client}
