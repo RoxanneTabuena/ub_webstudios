307ebdb933ca8c747b0d9574ec98630cca0527bf
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-25 08:34:53 -0700

    add menu_info content storage system

--- src/components/book/book.content.js
+++ src/components/book/book.content.js
@@ -1,1 +1,16 @@
-export const menu_info = {}
+export const menu_info = {
+    "basic promo + landing" : {
+        price : {
+            lumpSum : 700,
+            monthly: 100
+        },
+        labor : 3,
+    },
+    "translation services" : {
+        price: {
+            lumpSum: 100,
+            monthly: 10
+        },
+        labor: 1
+    }
+}
