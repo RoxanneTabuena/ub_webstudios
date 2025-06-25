488361593224c83164230cca16dcaf737a7da6c9
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-25 08:35:38 -0700

    control state for "promo" and "build"

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Menu } from './Menu'
 import { Promo } from './Promo'
 import { Build } from './Build'
@@ -6,13 +7,25 @@ import { menu_info } from './book.content'
 import style from './book.module.css'
 
 export const Book = () => {
+    const [promo, setPromo] = useState("basic promo + landing")
+    const [build, setBuild] = useState("basic promo + landing")
     const calculateCost = (build) => {
         return "build total"
     }    
     const estimateBuildTime = (build) => {
         return "estimated build deadline"
     }
-
+    const handleUpdatePromo = (item) => {
+        setPromo(item)
+    }
+    const handleAddBuild = (item) => {
+        setBuild([...build, item])
+    }
+    const handleRemoveBuild = (item) => {
+        setBuild(build.filter((i)=>{
+            return i !== item
+        }))
+    }
     return (
         <div className={style.book}>
             <Promo page={promo}/>
