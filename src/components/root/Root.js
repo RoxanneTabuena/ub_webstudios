5bde00db28c22d4b97db893407255e1468d65de9
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-24 17:30:32 -0700

    remove Book from Root

--- src/components/root/Root.js
+++ src/components/root/Root.js
@@ -3,14 +3,18 @@ import { useScrollMetrics } from '../../hooks/useScrollMetrics'
 import { useWindowDimensions } from '../../hooks/useWindowDimensions'
 import { useComponentHeight } from '../../hooks/useComponentHeight'
 import { ScrollToTop } from '../../hooks/ScrollToTop'
+import { useLocation } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
 import { Header } from './header/Header'
 import { Footer } from './Footer'
 import { Scroller} from './Scroller'
+import { Book } from '../book/Book'
 import style from "./root.module.css"
 
 
 export const Root = () => {
+    // get path
+    const {pathname} = useLocation()
     // header related variables
     const [headerRef, headerHeight] = useComponentHeight()
     // scroller related variables
@@ -27,35 +31,42 @@ export const Root = () => {
             top: ((e.clientY-headerHeight)*scrollHeight/(height))
         })
     }
+    console.log(pathname)
     return (
         <div className={style.root}>
-            <div 
-                className={style.scrollEvents}
-                style={{
-                    width: `${scrollWidth}px`,
-                    top: headerHeight
-                }}
-                onClick={handleScrollClick}
-            >
-            </div>
-            <ScrollToTop ref={mainRef}/>
-            <Scroller 
-                text="UNBOUNDSTUDIO" 
-                color="rgb(255,255,255)" 
-                totalHeight={scrollHeight} 
-                position={scrollTop}
-                handleWidthChange={handleWidthChange}
-                headerHeight={headerHeight}
-                />
-            <Header headerRef={headerRef} scrollY={scrollTop}/>
-            <main 
-                ref={mainRef}
-                style={{
-                    paddingTop: `${headerHeight}px`,
-                    }}>
-                <Outlet context={headerHeight} />
-            </main>
-            <Footer />
+            {pathname === '/book' ? 
+                <Book />
+                :
+                <div className={style.body}>
+                    <div 
+                        className={style.scrollEvents}
+                        style={{
+                            width: `${scrollWidth}px`,
+                            top: headerHeight
+                        }}
+                        onClick={handleScrollClick}
+                    >
+                    </div>
+                    <ScrollToTop ref={mainRef}/>
+                    <Scroller 
+                        text="UNBOUNDSTUDIO" 
+                        color="rgb(255,255,255)" 
+                        totalHeight={scrollHeight} 
+                        position={scrollTop}
+                        handleWidthChange={handleWidthChange}
+                        headerHeight={headerHeight}
+                        />
+                    <Header headerRef={headerRef} scrollY={scrollTop}/>
+                    <main 
+                        ref={mainRef}
+                        style={{
+                            paddingTop: `${headerHeight}px`,
+                            }}>
+                        <Outlet context={headerHeight} />
+                    </main>
+                    <Footer />
+                </div>
+            }
         </div>
     )
 }
