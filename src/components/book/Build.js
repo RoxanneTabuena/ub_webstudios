452ebb316594caa38985fa746fba037921af523b
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-25 10:56:01 -0700

    display "cost" and "time" using accurate computations

--- src/components/book/Build.js
+++ src/components/book/Build.js
@@ -1,21 +1,28 @@
+import { useState } from 'react'
 import { BuildItem } from './BuildItem'
+import { calculateCost, estimateTime } from './book.utils'
 import style from './book.module.css'
 
-export const Build = ({build, cost, time, handleUpdatePromo, handleRemoveBuild}) => {
-
-    // toggle payment plan / lump sum
+export const Build = ({build, handleUpdatePromo, handleRemoveBuild}) => {
+    const [format, setFormat] = useState("lumpSum")
+    const cost = calculateCost(build, format)
+    const time = estimateTime(build)
+    const handleSwitchFormat = () => {
+        setFormat(format === 'lumpSum' ? 'monthly' : 'lumpSum')
+    }
     return (
         <div className={style.build}>
             <h2>Build</h2>
             {build.map((item)=>{
             return <BuildItem 
                         key={item} 
-                        item={item} 
+                        item={item}
                         handleUpdatePromo={handleUpdatePromo}
                         handleRemoveBuild={handleRemoveBuild}/>
             })}
-            <p>{cost}</p>
-            <p>{time}</p>
+            <p>{`${format} total:${cost}`}</p>
+            <p>estimated build time: {time} days</p>
+            <p onClick={handleSwitchFormat}>switch pricing format</p>
         </div>
     )
 }
--- src/components/book/BuildContainer.js
+++ src/components/book/BuildContainer.js
@@ -1,14 +1,12 @@
 import style from './book.module.css'
 import { Build } from './Build'
 
-export const BuildContainer = ({build, cost, time, handleUpdatePromo, handleRemoveBuild}) => {
+export const BuildContainer = ({build, handleUpdatePromo, handleRemoveBuild}) => {
     // toggle payment plan / lump sum
     return (
         <div className={style.buildContainer}>
             <Build 
                 build={build} 
-                cost={cost} 
-                time={time}
                 handleUpdatePromo={handleUpdatePromo}
                 handleRemoveBuild={handleRemoveBuild}/>
         </div>
