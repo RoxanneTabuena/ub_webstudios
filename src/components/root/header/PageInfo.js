b897d4788af64ac71d49d59559eb5be0d943c20a
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-25 14:30:31 -0700

    set PageInfo font height according to "height" prop

--- src/components/root/header/PageInfo.js
+++ src/components/root/header/PageInfo.js
@@ -1,15 +1,21 @@
+import { useRef } from 'react'
 import { useLocation } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
 import style from './header.module.css'
 export const PageInfo = ({ height}) => {
     const {pathname} = useLocation()
     const title = pathname === '/' ? 'Welcome' : pathname.split('/')[1]
+    // set font height according to icon height
+    const letterHeight = useRef(height/2.3)
     return (
         <div className={style.pageInfo}
             style={{
                 height: height}}
-        > 
+        >
         <NavLink to={pathname !== '/' ? `/${title}` : '/'}>
-            <h2>{title.toUpperCase()}</h2>
+            <h2 style = {{
+                fontSize: letterHeight.current
+            }}
+            >{title.toUpperCase()}</h2>
         </NavLink>
         </div>)}
