c49e44e2aa32d6a2b3548c3973b04280a24a86e5
Roxanne Tabuena <roxyannet@gmail.com>
2025-06-25 10:25:07 -0700

    add close button

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Menu } from './Menu'
 import { Promo } from './Promo'
 import { BuildContainer } from './BuildContainer'
 import {PromoInfoContainer} from './PromoInfoContainer'
 import { menu_info } from './book.content'
+import closeIcon from '../../assets/img/Close_Icon.png'
 import style from './book.module.css'
 
 export const Book = () => {
     const [promo, setPromo] = useState("basic promo + landing")
     const [build, setBuild] = useState(["basic promo + landing"])
+    const navigate = useNavigate()
     const calculateCost = (build) => {
         return "build total"
     }    
@@ -26,12 +29,16 @@ export const Book = () => {
             return i !== item
         }))
     }
+    const close = () =>{
+        navigate(-1)
+    }
     // update promo on build change
     useEffect(()=>{
         setPromo(build.at(-1))
     }, [build])
     return (
         <div className={style.book}>
+            <img className={style.icon} src={closeIcon} alt="close Icon" onClick={close}></img>
             <Promo page={promo}/>
             <Menu menu={Object.keys(menu_info)} handleUpdatePromo={handleUpdatePromo}/>
             <BuildContainer build={build} cost={`cost`} time={`time`} handleRemoveBuild={handleRemoveBuild} handleUpdatePromo={handleUpdatePromo}/>
